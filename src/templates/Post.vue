e05dca7f04e8cc42154b7fa2b40ecaeadd2c9276
Mateus Ávila Isidoro <mateus@mateusavila.com.br>
2020-04-30 10:24:22 -0300

    fix in template post

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -8,7 +8,7 @@
                 </div>
             </div>
             <div class="row mt-4">
-                <div class="col-md-10 offset-md-1"><g-image :alt="'Image de destaque do artigo: ' + $page.post.title" class="single-post__image" :src="$page.post.featured_image" /></div>
+                <div class="col-md-10 offset-md-1"><g-image :alt="'Image de destaque do artigo: ' + $page.post.title" class="single-post__image" :src="'/'+$page.post.featured_image" /></div>
             </div>
             <div class="row mt-3">
                 <p class="col-md-10 offset-md-1 single-post__excerpt" v-html="$page.post.excerpt"></p>
